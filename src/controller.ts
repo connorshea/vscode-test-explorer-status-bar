f4f83e8cc936118e376256c37ac2afd389ba80dc
Connor Shea <connor.james.shea@gmail.com>
2019-06-08 12:29:18 -0600

    Rename controller and add some wip functionality for getting a test list.

--- src/controller.ts
+++ src/controller.ts
@@ -5,7 +5,7 @@ import { TestController, TestAdapter, TestSuiteInfo, TestInfo } from 'vscode-tes
  * This class is intended as a starting point for implementing a "real" TestController.
  * The file `README.md` contains further instructions.
  */
-export class ExampleController implements TestController {
+export class StatusBarController implements TestController {
 
   // here we collect subscriptions and other disposables that need
   // to be disposed when an adapter is unregistered
@@ -14,59 +14,55 @@ export class ExampleController implements TestController {
   private statusBarItem: vscode.StatusBarItem;
   private passedTests = 0;
   private failedTests = 0;
+  private testCount = 0;
+  private testSuite: TestSuiteInfo | undefined = undefined;
+  private testList: Array<TestInfo> | undefined = undefined;
 
   constructor() {
-    this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+    this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 5);
     this.statusBarItem.show();
 
-    // run all tests when the statusBarItem is clicked,
-    // we do this by invoking a command that is contributed by the Test Explorer extension
+    // Run all tests when the statusBarItem is clicked,
+    // We do this by invoking a command that is contributed by the Test Explorer extension
     this.statusBarItem.command = 'test-explorer.run-all';
   }
 
   registerTestAdapter(adapter: TestAdapter): void {
-    
     const adapterDisposables: { dispose(): void }[] = [];
     this.disposables.set(adapter, adapterDisposables);
-
-
-    // the ExampleController will simply listen for events from the Test Adapter(s)
-    // and write them to a StatusBarItem
 
     adapterDisposables.push(adapter.tests(testLoadEvent => {
       if (testLoadEvent.type === 'started') {
         this.statusBarItem.text = 'Loading tests...';
-
-      } else { // testLoadEvent.type === 'finished'
-
-        const rootSuite = testLoadEvent.suite;
-        const testCount = rootSuite ? countTests(rootSuite) : 0;
-        this.statusBarItem.text = `Loaded ${testCount} tests`;
-
+      } else {
+        this.testSuite = testLoadEvent.suite;
+        this.getTestSuiteInfo(testLoadEvent.suite);
+        this.testCount = this.testSuite ? countTests(this.testSuite) : 0;
+        this.statusBarItem.text = `Loaded ${this.testCount} tests`;
       }
     }));
 
     adapterDisposables.push(adapter.testStates(testRunEvent => {
       if (testRunEvent.type === 'started') {
-
         this.statusBarItem.text = 'Running tests: ...';
         this.passedTests = 0;
         this.failedTests = 0;
-
       } else if (testRunEvent.type === 'test') {
-
         if (testRunEvent.state === 'passed') {
           this.passedTests++;
         } else if (testRunEvent.state === 'failed') {
           this.failedTests++;
         }
-
-        this.statusBarItem.text = `Running tests: ${this.passedTests} passed / ${this.failedTests} failed`;
-
+        this.statusBarItem.text =
+          this.failedTests > 0
+            ? `Running tests: ${this.passedTests}/${this.testCount} passed (${this.failedTests} failed)`
+            : `Running tests: ${this.passedTests}/${this.testCount} passed`;
+      
       } else if (testRunEvent.type === 'finished') {
-
-        this.statusBarItem.text = `Tests: ${this.passedTests} passed / ${this.failedTests} failed`;
-
+        this.statusBarItem.text =
+          this.failedTests > 0
+            ? `Tests: ${this.passedTests}/${this.testCount} passed (${this.failedTests} failed)`
+            : `Tests: ${this.passedTests}/${this.testCount} passed`;
       }
     }));
   }
@@ -74,12 +70,43 @@ export class ExampleController implements TestController {
   unregisterTestAdapter(adapter: TestAdapter): void {
     const adapterDisposables = this.disposables.get(adapter);
     if (adapterDisposables) {
-
       for (const disposable of adapterDisposables) {
         disposable.dispose();
       }
-
       this.disposables.delete(adapter);
+    }
+  }
+
+  private getTestSuiteInfo(suite: TestSuiteInfo | undefined): any {
+    if (suite === undefined) {
+      return [];
+    }
+    
+    this.testList = this.generateTestList(suite);
+    console.log(this.testList);
+
+    return this.testList;
+  }
+
+  private generateTestList(info: TestSuiteInfo | TestInfo): any {
+    console.log(info);
+    if (info.type === 'suite') {
+      let testList: Array<any> = [];
+      for (const child of info.children) {
+        testList.concat(this.generateTestList(child));
+      }
+      return testList;
+    } else { // info.type === test
+      return {
+        id: info.id,
+        status: 'unknown'
+      }
+    }
+  }
+
+  private setTestState(testId: String, state: String): void {
+    if (this.testList !== undefined) {
+      // let test = this.testList.filter((test) => test.id === testId);
     }
   }
 }
@@ -91,7 +118,8 @@ function countTests(info: TestSuiteInfo | TestInfo): number {
       total += countTests(child);
     }
     return total;
-  } else { // info.type === 'test'
+  } else if (info.type === 'test') {
     return 1;
   }
+  return 0;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,18 @@
 import * as vscode from 'vscode';
 import { TestHub, testExplorerExtensionId } from 'vscode-test-adapter-api';
-import { ExampleController } from './controller';
+import { StatusBarController } from './controller';
 
 let testHub: TestHub | undefined;
-let controller: ExampleController | undefined;
+let controller: StatusBarController | undefined;
 
 export async function activate(context: vscode.ExtensionContext) {
-
   // get the Test Explorer extension
   const testExplorerExtension = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
 
   if (testExplorerExtension) {
-
     testHub = testExplorerExtension.exports;
-    controller = new ExampleController();
+    controller = new StatusBarController();
     testHub.registerTestController(controller);
-
   }
 }
 
